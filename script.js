304785756a7d1885297534b5dfdd9afa92fe4f74
clvalada <clvalada@gmail.com>
2024-01-05 14:16:51 -0500

    adding gameplay messaging

--- script.js
+++ script.js
@@ -98,19 +98,22 @@ function buyHealth () {
 }
 
 function buyWeapon() {
-    if (gold >= 30) {
-      gold -= 30;
-      currentWeapon++;
-      goldText.innerText = gold;
-      let newWeapon = weapons[currentWeapon].name;
-      text.innerText = "You now have a " + newWeapon + ".";
-      inventory.push(newWeapon);
-      text.innerText += " In your inventory you have: " + inventory.join;
+    if (currentWeapon < weapons.length - 1){
+        if (gold >= 30) {
+            gold -= 30;
+            currentWeapon++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeapon].name;
+            text.innerText = "You now have a " + newWeapon + ".";
+            inventory.push(newWeapon);
+            text.innerText += " In your inventory you have: " + inventory;
+        } else {
+        text.innerText = "You do not have enough gold to buy a weapon.";
     } else {
-        if (gold < 30) {
-          text.innerText = "You do not have enough gold to buy a weapon.";
-        }
+        text.innerText = "You already have the most powerful weapon!"
+    }
   }
+}
 
 function fightSlime(){
 
