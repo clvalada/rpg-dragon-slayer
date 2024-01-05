c7233e5e80b55bf91b4c5f2bec0da71fdf3cc571
clvalada <clvalada@gmail.com>
2024-01-05 14:09:06 -0500

    updating functions

--- script.js
+++ script.js
@@ -88,18 +88,29 @@ function fightDragon() {
 
 function buyHealth () {
     if (gold >= 10) {
-    gold -= 10;
-    health += 10; 
-    goldText.innerText = gold;
-    healthText.innerText = health;
-    } else {
+        gold -= 10;
+        health += 10; 
+        goldText.innerText = gold;
+        healthText.innerText = health;
+        } else {
         text.innerText = "You do not have enough gold to buy health."
       }
 }
 
 function buyWeapon() {
-
-}
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory.join;
+    } else {
+        if (gold < 30) {
+          text.innerText = "You do not have enough gold to buy a weapon.";
+        }
+  }
 
 function fightSlime(){
 
