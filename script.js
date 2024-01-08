af4eadb9ade8ea4e9b8793940c56ae023b8cd19d
clvalada <clvalada@gmail.com>
2024-01-08 13:00:17 -0500

    updated defeatMonsters function

--- script.js
+++ script.js
@@ -138,6 +138,10 @@ function dodge() {
 
 function defeatMonster() {
     gold += Math.floor(monsters[fighting].level * 6.7);
+    xp +=monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
 }
 
 function lose(){
