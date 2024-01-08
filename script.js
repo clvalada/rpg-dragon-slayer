ca00081ab61c91da01e42630b793d289234181a8
clvalada <clvalada@gmail.com>
2024-01-08 11:51:59 -0500

    updating functions

--- script.js
+++ script.js
@@ -98,21 +98,30 @@ function buyHealth () {
 }
 
 function buyWeapon() {
-    if (currentWeapon < weapons.length - 1){
-        if (gold >= 30) {
-            gold -= 30;
-            currentWeapon++;
-            goldText.innerText = gold;
-            let newWeapon = weapons[currentWeapon].name;
-            text.innerText = "You now have a " + newWeapon + ".";
-            inventory.push(newWeapon);
-            text.innerText += " In your inventory you have: " + inventory;
-        } else {
+    if (currentWeapon < weapons.length - 1) {
+      if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+      } else {
         text.innerText = "You do not have enough gold to buy a weapon.";
+      }
     } else {
-        text.innerText = "You already have the most powerful weapon!"
+      text.innerText = "You already have the most powerful weapon!";
+      button2.innerText = "Sell weapon for 15 gold";
+      button2.onclick = sellWeapon;
     }
   }
+
+function sellWeapon (){
+    if (inventory.length > 1);
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
 }
 
 function fightSlime(){
