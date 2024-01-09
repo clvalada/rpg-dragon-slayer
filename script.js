21326f0e5223e0f8144e8d0a08ce9081e5aceb0e
clvalada <clvalada@gmail.com>
2024-01-09 11:08:08 -0500

    added monsterAttackValue

--- script.js
+++ script.js
@@ -138,15 +138,21 @@ monsterHealthText.innerText = monsterHealth;
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-    health -= monsters[fighting].level;
+    health -= getMonsterAttackValue(monsters[fighting].level);
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
     if (health <= 0) {
-        lose();
+      lose();
     } else if (monsterHealth <= 0) {
-        fighting === 2 ? winGame() : defeatMonster();
+      fighting === 2 ? winGame() : defeatMonster();
     }
+  }
+
+function getMonsterAttackValue(level){
+    const hit = (level *5) - (Math.floor(Math.random() * xp));
+    console.log(hit);
+    return (hit > 0) ? hit : 0;
 }
 
 function dodge() {
